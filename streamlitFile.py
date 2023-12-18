02e943bdf49ba939ac32816c608729775b9ed8bc
Virenda-Kumar <154111120+Virenda-Kumar@users.noreply.github.com>
2023-12-18 09:51:56 +0530

    Update streamlitFile.py

--- streamlitFile.py
+++ streamlitFile.py
@@ -1,9 +1,20 @@
 import streamlit as st
-import pandas as pd
- 
+import streamlit.components.v1 as components
+
+# >>> import plotly.express as px
+# >>> fig = px.box(range(10))
+# >>> fig.write_html('test.html')
+
 st.write("""
 # My first app
 Hello *world!*
 Hynnah Baker 
 """)
+
+st.header("test html import")
+
+HtmlFile = open("calculator_UI.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.html(source_code)
  
